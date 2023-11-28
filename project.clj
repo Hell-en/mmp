8ab39f150f8db60a9c2641151351efca2e06d187
AnastPot <106525506+AnastPot@users.noreply.github.com>
2023-11-28 19:50:36 +0700

    Update project.clj

--- project.clj
+++ project.clj
@@ -17,7 +17,8 @@
 (defn second_id [n])
 (defn third_id [n])
 (defn walk [lst])
-(defn apply_func [filtr_lst])
+(defn conveyor [filtr_lst])
+(defn apply_func [filtr])
 
 (defn read_input []    (def x (read))
     (first_id x)    (def x (read))
@@ -28,7 +29,8 @@
 ; id запроса
 (defn first_id [n]
     (if (int? n)
-    (println n)))
+    (println n)
+    (println "Wrong request ID")))
 ; id фотографий
 (defn second_id [n]
    ; (map int? n)
@@ -42,7 +44,7 @@
     (def ind_open (.indexOf (apply str filtr_lst) "("))
     (def ind_close (.indexOf (apply str filtr_lst) ")"))
     (println ind_open, ind_close)
-    (if (and (= -1 ind_open) (= -1 ind_close)) (apply_func filtr_lst))
+    (if (and (= -1 ind_open) (= -1 ind_close)) (conveyor filtr_lst))
     (cond 
         (and (= -1 ind_open) (< -1 ind_close)) (println "Error (")
         (and (= -1 ind_close) (< -1 ind_open)) (println "Error )")
@@ -51,56 +53,62 @@
     :else ; ()! ; когда 2 скобки
 )
 
-(defn noise_reduction [lst]) ; default
+(defn noise_reduction_1 [lst]) ; default
 (defn noise_reduction_2 [lst])
 (defn noise_reduction_3 [lst])
 (defn blur [lst])
 
 (defn panorama_stitching [lst])
 (defn HDR_conversion [lst])
-(defn fn_to_one [expr]
-    (if (= expr "panorama_stitching")
-        (panorama_stitching [])
-    )
-    (if (= expr "HDR_conversion")
-        (HDR_conversion [])
-    )
-)
+(defn choosing_best [lst])
 
 (defn cutting_areas [lst])
 
 
-(defn apply_func [filtr_lst] ; без выбора лучшего. шумы по умолчанию
-    (println "list of filtrs = ", filtr_lst)
+(defn conveyor [filtr_lst] ; голова конвеера. Разбивает строку запроса на отдельные фильтры
+    ; где-то здесь нужна обработка строки на наличие фильтров, скобок и тд, 
+    ; тк дальше будет работа с 1 фильтром, а не со всей строкой
+    
+    ; здесь же проверка на 1 или несколько включений фильтров н в 1
+    ; !! не важно в каком порядке ввод был. мы в любом случае смотрим, чтобы
+    ; если фильтр был только 1 любой, то его и сделаем. если есть 2, то ошибка, если ни 1, то
+    ; ничего делать не будет. решение!
+    (def n_to_one (.indexOf (apply str filtr_lst) "panorama_stitching"))
+    (def n_to_one_2 (.indexOf (apply str filtr_lst) "HDR_conversion"))
+    (if (and (not (= n_to_one -1)) (not (= n_to_one_2 -1))) 
+        (println "Error panorama_stitching and HDR_conversion at the same time")
+    )
+
+
     ; проверки:
     ; такие проверки берут первый. если фильтр указан дважды то плевать, делаем 1 раз
 
     ; 0) из н в 1 стоит или на первом месте или его нет
-    (def n_to_one (.indexOf (apply str filtr_lst) "panorama_stitching"))
-    (def n_to_one_2 (.indexOf (apply str filtr_lst) "HDR_conversion"))
-    ; !! не важно в каком порядке ввод был. мы в любом случае смотрим, чтобы
-    ; если фильтр был только 1 любой, то его и сделаем. если есть 2 или ни 1, то
-    ; ничего делать не будет. решение!
-    (if (and (= n_to_one -1) (not (= n_to_one_2 -1))) 
-        (HDR_conversion [])
-    )
-    (if (and (not (= n_to_one -1)) (= n_to_one_2 -1))
-        (panorama_stitching [])
-    )
-
-    ; 1) все слова соответсвуют названиям фильтров
-    (if (some (fn [elem] (= elem "noise_reduction")) filtr_lst)
-        (noise_reduction [])
-    )
-    (if (some (fn [elem] (= elem "blur")) filtr_lst)
-        (blur [])
-    )
     
     ; 3) из 1 в н стоит или на последнем месте или его нет - просто в последний зайдем
     ; нет двух одновременно фильтров н в 1 ! - и так нет, тк нет выбора в подфункции
-    (if (some (fn [elem] (= elem "cutting_areas")) filtr_lst)
-        (cutting_areas [])
-    )
+    
+    (println "list of filtrs = ", filtr_lst)
+    (def rest_request (atom (vec filtr_lst)))
+    (while (not(empty? @rest_request))
+        ;(println (first @rest_request))
+        (apply_func (first @rest_request))
+        (swap! rest_request rest))  
+)
+
+(defn apply_func [filtr] ; без выбора лучшего. шумы по умолчанию
+    (println "filtr = ", filtr)
+
+
+    (cond
+        (= filtr "panorama_stitching") (panorama_stitching [])
+        (= filtr "HDR_conversion") (HDR_conversion [])
+        (= filtr "noise_reduction_1") (noise_reduction_1 [])
+        (= filtr "noise_reduction_2") (noise_reduction_2 [])
+        (= filtr "noise_reduction_3") (noise_reduction_3 [])
+        (= filtr "blur") (blur [])
+        (= filtr "cutting_areas") (cutting_areas [])
+        :else (println "Error filter name"))
 )
 
 (read_input)
